3081671e9836856db79e00d9c9ee385eb04a95ff
Riccardo <riccardopitzanti@gmail.com>
2023-05-06 14:02:37 +0200

    aggiunto cast

--- home/dettagliFilm.php
+++ home/dettagliFilm.php
@@ -7,27 +7,43 @@
             or die('Could not connect: ' . pg_last_error());
     $url1 = "https://api.themoviedb.org/3/movie/$film?api_key=1cf50e6248dc270629e802686245c2c8"; 
     $url2 = "https://api.themoviedb.org/3/movie/$film/translations?api_key=1cf50e6248dc270629e802686245c2c8"; 
+    $url3 = "https://api.themoviedb.org/3/movie/$film/credits?api_key=1cf50e6248dc270629e802686245c2c8"; 
     echo "<div id='dettagli'></div>";
     echo "<script>
     var php_var1 = '<?php echo $url1; ?>'; 
             php_var1 = php_var1.substring(11, php_var1.length - 4);
             var php_var2 = '<?php echo $url2; ?>'; 
             php_var2 = php_var2.substring(11, php_var2.length - 4);
+            var php_var3 = '<?php echo $url3; ?>'; 
+            php_var3 = php_var3.substring(11, php_var3.length - 4);
             Promise.all([
                 fetch(php_var1).then(resp => resp.json()),
-                fetch(php_var2).then(resp => resp.json())
+                fetch(php_var2).then(resp => resp.json()),
+                fetch(php_var3).then(resp => resp.json())
               ]).then(data => {
             console.log(data);
             var {title, poster_path, vote_average, overview, runtime, release_date, id} = data[0];
             overview=getOverview(data[1],overview);
+            var cast = getCast(data[2]);
         if(poster_path){
             document.getElementById('dettagli').innerHTML += '<br> <img src=\'https://image.tmdb.org/t/p/w500'+poster_path+'\' alt=\''+title+'\'><br><br>';
         } else{ 
             document.getElementById('dettagli').innerHTML += '<img src=\'http://via.placeholder.com/1080x1580\' alt=\''+title+'\'>';
             }
         document.getElementById('dettagli').innerHTML += 'Titolo: ' + title + ' <br> Durata: '+ runtime +' minuti <br> Voto: '+ vote_average.toFixed(1) +'<br> Trama: ' +overview+'<br>';
+        document.getElementById('dettagli').innerHTML += 'Cast: ' + cast + '<br><br>';
         document.getElementById('dettagli').innerHTML += '<a href=\'MovieLand.php\'>Scegli un altro film</a><br><br>';
     });    
+
+    function getCast(data){
+        var {cast, crew} = data;
+        var returnedstring = '';
+        for(let act in cast){
+            var {name, character, id} = cast[act];
+            returnedstring += name + '(' + character + '), ';
+        }
+        return returnedstring;
+    }
 
     function getOverview(data, trama){
         const {id, translations} = data;
