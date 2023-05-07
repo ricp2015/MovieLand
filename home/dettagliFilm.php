0389dc4b65bb42cce722ba9b85a6ae641bcd2c40
Riccardo <riccardopitzanti@gmail.com>
2023-05-07 16:18:43 +0200

    aggiunte piattaforme

--- home/dettagliFilm.php
+++ home/dettagliFilm.php
@@ -6,5 +6,9 @@
             user=postgres password=password") 
             or die('Could not connect: ' . pg_last_error());
     echo "<div id='dettagli'></div>";
+    echo "<div id='providers'></div>";
+    echo "<script src='https://code.jquery.com/jquery-3.3.1.min.js'
+    integrity='sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8='
+    crossorigin='anonymous'> </script>";
     echo "<script type='text/javascript' src='scriptdettagli.js'></script>";
 ?>
--- home/scriptdettagli.js
+++ home/scriptdettagli.js
@@ -1,10 +1,10 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const film = urlParams.get('movie');
+var film = urlParams.get('movie');
 url1 = "https://api.themoviedb.org/3/movie/"+film+"?api_key=1cf50e6248dc270629e802686245c2c8"; 
 url2 = "https://api.themoviedb.org/3/movie/"+film+"/translations?api_key=1cf50e6248dc270629e802686245c2c8"; 
 url3 = "https://api.themoviedb.org/3/movie/"+film+"/credits?api_key=1cf50e6248dc270629e802686245c2c8"; 
-url4 = "https://api.themoviedb.org/3/movie/"+film+"watch/providers?api_key=1cf50e6248dc270629e802686245c2c8"; 
+url4 = "https://api.themoviedb.org/3/movie/"+film+"/watch/providers?api_key=1cf50e6248dc270629e802686245c2c8"; 
 Promise.all([
     fetch(url1).then(resp => resp.json()),
     fetch(url2).then(resp => resp.json()),
@@ -15,16 +15,47 @@ Promise.all([
             var {title, poster_path, vote_average, overview, runtime, release_date, id} = data[0];
             var overview=getOverview(data[1],overview);
             var cast = getCast(data[2]);
+            var images = getProviders(data[3]);
         if(poster_path){
-            document.getElementById('dettagli').innerHTML += '<br> <img src=\'https://image.tmdb.org/t/p/w500'+poster_path+'\' alt=\''+title+'\'><br><br>';
+            document.getElementById('dettagli').innerHTML += '<br> <img src=\'https://image.tmdb.org/t/p/original'+poster_path+'\' alt=\''+title+'\' height=\'720\' width=\'480\'><br><br>';
         } else{ 
             document.getElementById('dettagli').innerHTML += '<img src=\'http://via.placeholder.com/1080x1580\' alt=\''+title+'\'>';
             }
         document.getElementById('dettagli').innerHTML += 'Titolo: ' + title + ' <br> Durata: '+ runtime +' minuti <br> Voto: '+ vote_average.toFixed(1) +'<br> Trama: ' +overview+'<br>';
         document.getElementById('dettagli').innerHTML += 'Cast: ' + cast + '<br><br>';
         document.getElementById('dettagli').innerHTML += '<a href=\'MovieLand.php\'>Scegli un altro film</a><br><br>';
+        var cont = 0;
+        document.getElementById('providers').innerHTML += "Guardalo su: <br>";
+        for(let i in images){
+            cont++;
+            var img = document.createElement('img');
+            img.src = "https://image.tmdb.org/t/p/original" + images[i];
+            img.width = "50";
+            img.height = "50";
+            document.getElementById('providers').appendChild(img);
+        }
+        if(cont == 0){
+            document.getElementById('providers').innerHTML += "Il film attualmente non è disponibile su nessun servizio di streaming";
+        }
     });    
 
+    function getProviders(data){
+        var {results} = data;
+        var images = [];
+        if(!$.isEmptyObject(results) && "IT" in results){
+        res = results.IT;
+        for(let i in res.flatrate){
+            images.push(res.flatrate[i].logo_path);
+                    }
+        for(let j in res.buy){
+            images.push(res.buy[j].logo_path);
+                }
+        for(let k in res.rent){
+            images.push(res.rent[k].logo_path);
+        }
+    }
+        return [...new Set(images)];
+    }
 
     function getCast(data){
         var {cast, crew} = data;
@@ -37,7 +68,7 @@ Promise.all([
     }
 
     function getOverview(data, trama){
-        const {id, translations} = data;
+        const {translations} = data;
         var overview = '';
         for(let i in translations){
             if(translations[i]['iso_639_1']=='it'){
