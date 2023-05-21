935764a71db9e0505555883c8e584ddf3a06b5c7
BlackGrampa <alessio.celli25@gmail.com>
2023-05-21 17:25:51 +0200

    css review e watchlist

--- home/fetchWatchlists.php
+++ home/fetchWatchlists.php
@@ -11,25 +11,60 @@ if ($dbconn) {
     $q1 = "select * from watchlist where utente = $1";
     $result = pg_query_params($dbconn, $q1, array($email));
     $counter = 0;
+    echo "<!DOCTYPE html>
+            <html lang='it'>
+                <head>
+                    <meta charset='UTF-8'>
+                    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                    <link rel='stylesheet' type='text/css' href='../bootstrap/css/bootstrap.css'/>
+                    <script type='application/javascript' src='../bootstrap/js/bootstrap.min.js'></script>
+                    <link rel='stylesheet' href='elencoWatchlist.css'>
+                    <title>Elenco Watchlist</title>
+                </head>
+                <body class='watchlist-list'>";
     while ($row = pg_fetch_assoc($result)) {
         $counter++;
         $nomew = $row['nome'];
         $descrizionew = $row['descrizione'];
         $watch = $row['id'];
-        echo "$nomew: $descrizionew", PHP_EOL;
+        //echo "$nomew: $descrizionew", PHP_EOL;
         if(isset($_GET["movie"]) and isset($_GET["title"])){
             $movie = $_GET['movie'];
             $title = $_GET['title'];
             $title = urlencode($title);
-            echo "<form action='addToWatchlist.php' method='get'>
+            /*echo "<form action='addToWatchlist.php' method='get'>
             <input type='hidden' id='movie' name='movie' value='$movie'> 
             <input type='hidden' id='title' name='title' value='$title'> 
             <input type='hidden' id='watchlist' name='watchlist' value='$watch'> 
-            <input type='submit' value='Aggiungi a questa Watchlist'> </form>";
+            <input type='submit' value='Aggiungi a questa Watchlist'> </form>";*/
+            echo "  <div class='watchlist'>
+                        <div class='watchlist-title'>
+                            <h3 class='name-description'>$nomew: $descrizionew</h3>
+                        </div>
+                        <div class='addToWatchlist-form'>
+                            <form action='addToWatchlist.php' method='get'>
+                                <input type='hidden' id='movie' name='movie' value='$movie'> 
+                                <input type='hidden' id='title' name='title' value='$title'> 
+                                <input type='hidden' id='watchlist' name='watchlist' value='$watch'> 
+                                <button type='submit' value='Aggiungi a questa Watchlist' class='btn btn-primary'>Aggiungi a questa Watchlist</button>
+                            </form>
+                        </div>
+                    </div>";
         } else{
-            echo "<form action='viewWatchlist.php' method='get'>
+            /*echo "<form action='viewWatchlist.php' method='get'>
             <input type='hidden' id='watchlist' name='watchlist' value='$watch'> 
-            <input type='submit' value='Consulta'> </form>";
+            <input type='submit' value='Consulta'> </form>";*/
+            echo "  <div class='watchlist'>
+                        <div class='watchlist-title'>
+                            <h3 class='name-description'>$nomew: $descrizionew</h3>
+                        </div>
+                        <div class='viewWatchlist-form'>
+                            <form action='viewWatchlist.php' method='get'>
+                                <input type='hidden' id='watchlist' name='watchlist' value='$watch'> 
+                                <button type='submit' value='Consulta' class='btn btn-primary'>Consulta</button>
+                            </form>
+                        </div>
+                    </div>";
         }
     }
     if ($counter == 0) {
@@ -37,7 +72,13 @@ if ($dbconn) {
         //    <a href=creaWatchlist.html> Clicca qui per crearne una </a>";
         header("Location:fetchWatchlists.html");
         exit();
-    }else{echo "<a href=creaWatchlist.html> Clicca qui per creare una nuova watchlist</a>";
+    }else{
+        echo "      <div class='button-div'>
+                        <a href='creaWatchlist.html'><button class='newWatchlist btn btn-primary'>Crea una nuova watchlist</button></a>
+                    </div>
+                </body>
+            </html>";
+        //echo "<a href=creaWatchlist.html> Clicca qui per creare una nuova watchlist</a>";
     }
 }
 ?>
